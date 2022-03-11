2af5f47872f6755fc6483144fd06a3253996993e
Quang Vu <vhquang@gmail.com>
2022-03-11 00:40:23 -0800

    Use absolute dir path for config

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+FULL_PATH="$(realpath "$0")"
+DIR_PATH="$(dirname "$FULL_PATH")"
+
 docker run -d --rm \
        -e DISPLAY=unix$DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -v $(pwd)/.config:/home/developer/.config \
+       -v "$DIR_PATH"/.config:/home/developer/.config \
        pomodone-docker 
